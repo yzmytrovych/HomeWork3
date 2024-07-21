fb148438d40d9d4c4b70a1a235e64d56f91714e7
Julia Zmytrovych <97659424+YuliyaZmytrovych@users.noreply.github.com>
2024-07-21 20:20:22 +0300

    homework 3

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('Hey, this is my JS program'); 
+const num = 3;
+if(num % 3)
+    console.log('Fizz'); 
+if(num % 5)
+    console.log('Buzz'); 
+if(num % 3 && num % 5)
+    console.log('FizzBuzz'); 
+
+const year = 2024;
+if(year%4==0 && (year%100!=0 || year%400==0)
+    console.log('leap year'); 
+else
+    console.log('not leap year'); 
+
+const yourYear = 30;
+console.log(yourYear); 
+if(yourYear % 1)
+    console.log('рік'); 
+if(yourYear < 2 && yourYear < 41)
+    console.log('років'); 
+if(yourYear > 41)
+    console.log('роки'); 
